de8f8328b50bb35a0b17f2ea2d30297211292a77
Carlos <cagalmarini@hotmail.com>
2022-01-01 16:06:21 -0300

    Update stats.py

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -17,6 +17,6 @@ def written():
 @hooks.register("stats")
 def stats():
     final_time = time.time() - start_time
-    average = final_time / total_written if total_written != 0 else None
-
-    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file".format(total_written, final_time, average)
+    average = final_time / total_written if total_written != 0 else 0
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
+    print(report.format(total_written, final_time, average))
